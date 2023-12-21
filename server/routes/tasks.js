201e739998471ebf234533a39249b2319e956f1e
Alise Cervantes <alise09@icloud.com>
2023-12-20 17:29:00 -0800

    testing done

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose"); // Make sure to require mongoose
 const router = express.Router();
 const Task = require("../models/task");
 
 // POST route for creating a task
 router.post("/tasks", async (req, res) => {
-  const { userId, title, description, deadline, priority } = req.body; // Destructure required fields from body
+  const { userId, title, description, deadline, priority } = req.body;
 
   try {
     const newTask = new Task({
@@ -12,7 +13,7 @@ router.post("/tasks", async (req, res) => {
       description,
       deadline,
       priority,
-      user: userId // Set the user field with the provided userId
+      user: userId,
     });
     await newTask.save();
     res.status(201).send(newTask);
@@ -23,13 +24,20 @@ router.post("/tasks", async (req, res) => {
 
 // GET route to fetch tasks for a specific user
 router.get("/tasks/user/:userId", async (req, res) => {
+  console.log("Requested User ID:", req.params.userId); // Log the User ID from the URL
   try {
+    const validUserId = mongoose.Types.ObjectId.isValid(req.params.userId);
+    console.log("Is User ID valid?", validUserId); // Log the validity of the User ID
+    if (!validUserId) {
+      return res.status(400).send("Invalid user ID format");
+    }
     const tasks = await Task.find({ user: req.params.userId });
+    console.log("Tasks found:", tasks); // Log the tasks found
     res.json(tasks);
   } catch (error) {
+    console.error("Error fetching tasks:", error); // Log any errors
     res.status(500).send(error);
   }
 });
-
 
 module.exports = router;
