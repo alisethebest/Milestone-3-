a8606e721bf705e159e3ac42b7185ef2803beb31
Alise Cervantes <alise09@icloud.com>
2023-12-20 23:06:53 -0800

    build

--- server/server.js
+++ server/server.js
@@ -1,19 +1,34 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
-const taskRoutes = require("./routes/tasks");
 const mongoose = require("mongoose");
+const path = require("path");
+const taskRoutes = require("./routes/tasks");
 const userRoutes = require("./routes/users");
+
+const app = express();
 
 app.use(express.json());
 app.use("/api", taskRoutes);
 app.use("/api", userRoutes);
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // Serve the React app
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
