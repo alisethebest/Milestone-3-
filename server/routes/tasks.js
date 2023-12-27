78a83b2a568bfd0955852f29856139d36515f5db
Alise Cervantes <alise09@icloud.com>
2023-12-27 13:41:44 -0800

    error on Adding Tasks

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,19 +4,14 @@ const router = express.Router();
 const Task = require("../models/task");
 
 // POST route for creating a task
-router.post("/add", async (req, res) => {
+router.post('/add', async (req, res) => {
   const { title, description, user } = req.body;
-  if (!title || !description || !user) {
-    return res.status(400).json({ error: "Missing task details" });
-  }
-
   try {
     const newTask = new Task({ title, description, user });
     await newTask.save();
     res.status(201).json(newTask);
   } catch (error) {
-    console.error("Error creating task:", error);
-    res.status(500).json({ error: "Failed to create task. Please try again." });
+    res.status(500).json({ message: "Failed to create task", error: error });
   }
 });
 
